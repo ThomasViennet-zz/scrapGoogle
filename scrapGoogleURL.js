5bde8c9dfff387a8ec109cc3d548693a47a52a8d
ThomasViennet <viennet.t@gmail.com>
2022-05-04 11:38:14 +0200

    add wait

--- scrapGoogleURL.js
+++ scrapGoogleURL.js
@@ -5,19 +5,25 @@ const browser = await puppeteer.launch({headless:true});
 const page = await browser.newPage();
 const navigationPromise = page.waitForNavigation();
 // await page.setViewport({ width: 2000, height: 836 });
+console.log('Navigateur ouvert');
 
 let serpAll = [];
+let wait = (Math.floor(Math.random() * 4) + 2) * 1000;
 
 await page.goto('https://www.google.com/');
 await page.waitForSelector('#L2AGLb > div');
 await page.click('#L2AGLb > div');
 // await page.waitFor(1000);
+console.log('Début du scrap');
 
-var queries = ['Query'];
+var queries = ['query'];
+
 
   for (let query of queries){
     await page.goto('https://www.google.com/search?q=' + query + '');
-    await page.waitForTimeout(2000);
+    console.log('Pause de ' + wait/1000 + ' secondes');
+    await page.waitForTimeout(wait);
+
 
     let serp = await page.evaluate(() => {
         let data = [];
@@ -33,11 +39,18 @@ var queries = ['Query'];
     serpAll.push(query + "," + serp);
     i = queries.indexOf(query);
     i++;
-    console.log('Mot-clé : ' + query + ' [' + i  + '/' + queries.length + ']');
+    console.log('Scrap de "' + query + '" [' + i  + '/' + queries.length + ']');
+    if(Number.isInteger(i/10))
+    {
+      console.log('Pause suppélmentaire de ' + wait/1000 + ' secondes');
+      await page.waitForTimeout(wait);
+    }
   }
-
+console.log('Fin du scrap');
 console.dir(serpAll, {'maxArrayLength': null});
 
+await page.waitForTimeout(wait);
 await navigationPromise
 await browser.close()
+console.log('Navigateur fermé');
 })()
